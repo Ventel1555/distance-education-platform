3f2f36d811c1d7ebd88b4516724e2577a00be5bd
Ventel1555 <ventelorp@gmail.com>
2023-09-28 23:38:48 +0300

    new start of the project (setup)

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for small_crm_system project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
