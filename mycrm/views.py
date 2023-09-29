da37f74c8a0c9c3905fe4737afa77816983dcc3e
Ventel1555 <ventelorp@gmail.com>
2023-09-29 20:16:31 +0300

    setup (nothing important)

--- mycrm/views.py
+++ mycrm/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+# в дальнейшем использовать LoginRequiredMixin
+def home(request):
+    return render(request, "base.html")
