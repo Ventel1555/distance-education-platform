cfb7ce25defe0e67305f4fb099633dc6511208a7
Ventel1555 <ventelorp@gmail.com>
2023-12-04 12:34:20 +0300

    first prototype of import users

--- edu_admin_tools/views.py
+++ edu_admin_tools/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 # from django.contrib.auth.mixins import LoginRequiredMixin
-from tablib import Dataset
-from .resources import UserResources
+import pandas as pd
 from users.models import User
-
 
 def add_schoolers(request):
     if request.method == 'POST':
-        pass    
+        excel_file = request.FILES['excel_file']
+        df = pd.read_excel(excel_file)
+        
+        for index, row in df.iterrows():
+            username = row.get('Логин')
+            name = row.get('Имя')
+            surname = row.get('Фамилия')
+            password = row.get('Пароль')
+            patronymic = row.get('Отчество')
+            if not isinstance(patronymic, str):
+                patronymic = ''
+            if username and password and name and surname:
+                User.objects.create_user(login=username, password=password, first_name=name, last_name=surname, patronymic=patronymic)
+        
+        return redirect('home')
+    
     return render(request, "tools/add_schoolers.html")  
