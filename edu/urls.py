e6031ccb252d0a97f83f7c0f2b14815bd72b94e5
Ventel1555 <ventelorp@gmail.com>
2024-03-17 12:44:06 +0300

    html code fixes

--- edu/urls.py
+++ edu/urls.py
@@ -6,8 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('edu/detail-lesson/<int:pk>', views.DetailLessonView.as_view(template_name="detail_lesson.html"), name='detail_lesson'),
     path('students/homework-list', views.list_homework_view, name='homework-list'),
-    path('students/info', views.students, name='for_students'),
-    path('teachers/info', views.teachers, name='for_teachers'),
     path('teachers/redaction', views.redaction, name='redaction_teachers'),
-    path('admin-panel/info', views.admin_panel, name='for_admin'),
 ]
